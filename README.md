18a36b0a00e94c2654947a9947d2b1f5ab3a10a0
= <=>
2022-04-05 17:18:18 +0100

    morse v1.0

---
+++ README.md
@@ -0,0 +1,2 @@
+Morse Crypt 
+Who knows, you might use it to chat with your babe, given she is smart enough, LOL
---
+++ morse_code.py
@@ -0,0 +1,144 @@
+# Coding Challenge 2
+# Name:
+# Student No:
+
+# A Morse code encoder/decoder
+
+MORSE_CODE = (
+    ("-...", "B"), (".-", "A"), ("-.-.", "C"), ("-..", "D"), (".", "E"), ("..-.", "F"), ("--.", "G"),
+    ("....", "H"), ("..", "I"), (".---", "J"), ("-.-", "K"), (".-..", "L"), ("--", "M"), ("-.", "N"),
+    ("---", "O"), (".--.", "P"), ("--.-", "Q"), (".-.", "R"), ("...", "S"), ("-", "T"), ("..-", "U"),
+    ("...-", "V"), (".--", "W"), ("-..-", "X"), ("-.--", "Y"), ("--..", "Z"), (".-.-.-", "."),
+    ("-----", "0"), (".----", "1"), ("..---", "2"), ("...--", "3"), ("....-", "4"), (".....", "5"), 
+    ("-....", "6"), ("--...", "7"), ("---..", "8"), ("----.", "9"), ("-.--.", "("), ("-.--.-", ")"),
+    (".-...", "&"), ("---...", ":"), ("-.-.-.", ";"), ("-...-", "="), (".-.-.", "+"), ("-....-", "-"),
+    ("..--.-", "_"), (".-..-.", '"'), ("...-..-", "$"), (".--.-.", "@"), ("..--..", "?"), ("-.-.--", "!")
+)
+
+# morse = '-... .- -... .-   --. --- -..'
+morse = 'BABA GOD'
+
+
+
+
+
+
+def print_intro():
+    print("Welcome to Wolmorse")
+    print("This program encodes and decodes Morse code.")
+
+
+def get_input():
+    while True:
+        mode = input('Would you like to encode (e) or decode (d) : ')
+        if mode == 'e':
+            message = input('What message would you like to encode : ')
+            break
+        elif mode == 'd':
+            message = input('What message would you like to decode : ')
+            break
+        else:
+            print('Invalid mode')
+    
+    return (mode, message)
+
+
+def encode(message):
+    morse_words = message.split(' ')
+    morse_string = []
+    for morse_word in morse_words:
+        morse_code = []
+        for morse_char in morse_word:
+            for morse_map in MORSE_CODE:
+                if morse_char in morse_map:
+                    morse_code.append(morse_map[0])
+        morse_string.append(' '.join(morse_code))
+
+    encoded_str = '   '.join(morse_string)
+    return encoded_str
+
+
+def decode(message):
+    morse_words = message.split('   ')
+    morse_string = []
+    for word in morse_words:
+        morse_chars = word.split(' ')
+        morse_alpha = ''
+
+        for morse_char in morse_chars:
+            for morse_map in MORSE_CODE:
+                if morse_char in morse_map:
+                    morse_alpha += morse_map[1]
+        morse_string.append(morse_alpha)
+                
+    decoded_str = ' '.join(morse_string)
+    return decoded_str
+
+# ---------- Challenge Functions (Optional) ----------
+
+
+def process_lines(filename, mode):
+    pass
+
+
+def write_lines(lines):
+    pass
+
+
+def check_file_exists(filename):
+    pass
+
+
+"""
+MAIN DRIVER FUNCTION
+----------------------------------------------------------------------------------------------
+Requirements:
+    • Prompt users to select a mode: encode (e) or decode (d).
+    • Check if the mode the user entered is valid.
+    If not, continue to prompt the user until a valid mode is selected.
+    • Prompt the user for the message they would like to encode/decode.
+    • Encode/decode the message as appropriate and print the output.
+    • Prompt the user whether they would like to encode/decode another message.
+        • Check if the user has entered a valid input (y/n).
+          If not, continue to prompt the user until they enter a valid response.
+          Depending upon the response you should either:
+            • End the program if the user selects no.
+            • Proceed directly to step 2 if the user says yes.
+    • Your program should be as close as possible to the example shown in the assessment specification.
+
+Hints:
+    • Use the tuple MORSE_CODE above to convert between plain text/Morse code
+    • You can make use of str.split() to generate a list of Morse words and characters
+      by using the spaces between words and characters as a separator.
+    • You will also find str.join() useful for constructing a string from a list of strings.
+    • You should use a loop to keep the programming running if the user says that would like to
+      encode/decode another message after the first.
+    • Your program should handle both uppercase and lowercase inputs. You can make use of str.upper()
+      and str.lower() to convert a message to that case.
+    • Check the assessment specification for code examples.
+"""
+def main():
+    while True:
+        (mode, message) = get_input()
+        if mode == 'e':
+            result = encode(message)
+            print(result)
+
+        elif mode == 'd':
+            result = decode(message)
+            print(result)
+
+        choice = input('Would you like to encode/decode another message? (y/n) : ')
+        if choice in ('n', 'N'):
+            break
+        elif choice in ('y', 'Y'):
+            main()
+        else: 
+            print('Invalid response')
+        
+
+
+
+# Program execution begins here
+if __name__ == '__main__':
+    main()
